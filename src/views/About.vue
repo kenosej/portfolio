6c1ad9b76753fb22803fca2c7eb46aca6bc62b5d
keno <kenosej.business@gmail.com>
2020-09-24 12:54:10 +0200

    Remove useless keep-alive

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,60 +1,54 @@
 <template>
-    <div id="about">
-        <keep-alive>
-            <Bio/>
-        </keep-alive>
-        <keep-alive>
-            <Info/>
-        </keep-alive>
-        <keep-alive>
-            <Mail/>
-        </keep-alive>
-    </div>
+  <div id="about">
+    <Bio />
+    <Info />
+    <Mail />
+  </div>
 </template>
 
 <script>
-    import Bio from "../components/about/Bio";
-    import Info from "../components/about/Info";
-    import Mail from "../components/about/Mail";
+import Bio from "../components/about/Bio";
+import Info from "../components/about/Info";
+import Mail from "../components/about/Mail";
 
-    export default {
-        name: "Services",
-        components: {
-            Bio,
-            Info,
-            Mail
-        }
-    }
+export default {
+  name: "Services",
+  components: {
+    Bio,
+    Info,
+    Mail,
+  },
+};
 </script>
 
 <style>
-    #about {
-        min-height: 863px;
-        display: grid;
-        grid-auto-rows: 623px 160px 107px;
-        width: 100%;
-    }
+#about {
+  min-height: 863px;
+  display: grid;
+  grid-auto-rows: 623px 160px 107px;
+  width: 100%;
+}
 
-    @media only screen and (min-width: 768px) {
-        #about {
-            height: 90vh;
-            grid-auto-rows: 75% 15% 10%;
-            min-height: 0;
-        }
-    }
+@media only screen and (min-width: 768px) {
+  #about {
+    height: 90vh;
+    grid-auto-rows: 75% 15% 10%;
+    min-height: 0;
+  }
+}
 
-    @media only screen and (max-width: 830px) and (min-width: 768px) and (orientation: landscape) {
-        #about {
-            height: 100%;
-            min-height: 833px;
-        }
-    }
+@media only screen and (max-width: 830px) and (min-width: 768px) and (orientation: landscape) {
+  #about {
+    height: 100%;
+    min-height: 833px;
+  }
+}
 
-    @media only screen and (min-width: 1200px) {
-        #about {
-            grid-template-rows: 80vh 10vh;
-            grid-template-columns: repeat(3, 1fr);
-            grid-template-areas: "bio bio bio" "info mail info2";
-        }
-    }
+@media only screen and (min-width: 1200px) {
+  #about {
+    grid-template-rows: 80vh 10vh;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-areas: "bio bio bio" "info mail info2";
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,29 @@
 <template>
-    <div class="home">
-        <keep-alive>
-            <Middle/>
-        </keep-alive>
-        <keep-alive>
-            <Footer/>
-        </keep-alive>
-    </div>
+  <div class="home">
+    <Middle />
+    <Footer />
+  </div>
 </template>
 
 <script>
-    import Middle from "../components/home/Middle";
-    import Footer from "../components/home/Footer";
+import Middle from "../components/home/Middle";
+import Footer from "../components/home/Footer";
 
-    export default {
-        name: "Home",
-        components: {
-            Middle,
-            Footer
-        }
-    }
+export default {
+  name: "Home",
+  components: {
+    Middle,
+    Footer,
+  },
+};
 </script>
 
 <style>
-    .home {
-        display: grid;
-        height: 90vh;
-        width: 100%;
-        overflow: hidden;
-        grid-template-rows: 70vh 20vh;
-    }
+.home {
+  display: grid;
+  height: 90vh;
+  width: 100%;
+  overflow: hidden;
+  grid-template-rows: 70vh 20vh;
+}
 </style>
